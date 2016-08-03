e90d56200b073e65e24d46d36e47c45235f5ace1
Brandon Richey <brichey@teladoc.com>
2016-08-03 14:24:38 -0400

    Adds some more complicated sub-components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import HelloWorld from './HelloWorld';
+
 const App = () => {
-  return (<div className="App">Hello World!</div>);
+  return (
+    <div className="App">
+      <HelloWorld name="Jim"/>
+      <HelloWorld name="Sally"/>
+    </div>
+  );
 };
 
 export default App;
